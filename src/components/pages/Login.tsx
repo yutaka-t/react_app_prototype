b06b729051a1fed95c3b4536131dbb42aa5c0f3e
Yutaka <yutaka.tanoue@gmail.com>
2022-01-10 17:37:04 +0900

    ボタンのコンポーネント化

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,4 +1,23 @@
+import { Divider, Flex, Heading, Box, Input, Stack } from "@chakra-ui/react";
 import { VFC, memo } from "react";
+
+import { PrimaryButton } from "../../components/atoms/button/PrimaryButton";
+
 export const Login: VFC = memo(() => {
-  return <p>ログインページです</p>;
+  return (
+    <Flex align="cneter" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザ管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="ユーザID" />
+
+          {/* コンポーネントにchildrenを渡している */}
+          <PrimaryButton>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
