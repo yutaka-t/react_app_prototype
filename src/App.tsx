4f721f70f67f59e4735876b6179aaa70e5651180
Yutaka <yutaka.tanoue@gmail.com>
2022-01-10 13:58:02 +0900

    バックアップ

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Button, ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
 import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-        <Button colorScheme="teal">ボタン</Button>
-        <p>aaaaあ</p>
+        <Router />
       </BrowserRouter>
     </ChakraProvider>
   );
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,13 +1,32 @@
 import { VFC, memo } from "react";
 import { Switch, Route } from "react-router-dom";
 import { Login } from "../components/pages/Login";
+import { HomeRoutes } from "./HomeRoutes";
 
 export const Router: VFC = memo(() => {
   return (
-<Switch>
-  <Route exact path="/">
-    <Login>
-  </Route>
-</Switch>
-  )
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
+      <Route
+        path="/home"
+        render={({ match: { url } }) => (
+          <Switch>
+            {HomeRoutes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      >
+        <Switch></Switch>
+      </Route>
+    </Switch>
+  );
 });
