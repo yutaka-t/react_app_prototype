2cb35ef394473511f9db712d7d3ea5f2a642f856
Yutaka <yutaka.tanoue@gmail.com>
2022-01-10 16:39:40 +0900

    バックアップ

---
+++ src/components/templates/HeaderLayout.tsx
@@ -0,0 +1,16 @@
+import { VFC, memo, ReactNode } from "react";
+import { Header } from "../../components/organisms/layout/Header";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const HeaderLayout: VFC<Props> = memo((props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+});
