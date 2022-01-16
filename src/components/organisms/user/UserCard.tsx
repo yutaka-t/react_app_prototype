c6b93ce95b1bac17b091ad1a98a6c78ea7206ba6
Yutaka <yutaka.tanoue@gmail.com>
2022-01-16 13:13:04 +0900

    backup

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,41 @@
+import { VFC, memo } from "react";
+import { Box, Stack, Image, Text } from "@chakra-ui/react";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo((props: Props) => {
+  const { imageUrl, userName, fullName } = props;
+  return (
+    <Box
+      w="260px"
+      h="260px"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      <Stack textAlign="center">
+        {/* @chakra-ui の Image タグである点に注意 */}
+        <Image
+          borderRadius="full"
+          boxSize="160px"
+          src={imageUrl}
+          alt={userName}
+          // マージン 中央揃えで設定
+          m="auto"
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,40 +1,37 @@
-import { VFC, memo } from "react";
-import { WrapItem, Wrap, Box, Stack, Image, Text } from "@chakra-ui/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { VFC, memo, useEffect } from "react";
+import { WrapItem, Wrap, Spinner, Center } from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  const sampleImageUrl = "https://source.unsplash.com/random";
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <>
-      <Wrap p={{ base: 4, md: 10 }}>
-        <WrapItem>
-          <Box
-            w="260px"
-            h="260px"
-            bg="white"
-            borderRadius="10px"
-            shadow="md"
-            p={4}
-            _hover={{ cursor: "pointer", opacity: 0.8 }}
-          >
-            <Stack textAlign="center">
-              {/* @chakra-ui の Image タグである点に注意 */}
-              <Image
-                borderRadius="full"
-                boxSize="160px"
-                src="https://source.unsplash.com/random"
-                alt="プロフィール画像"
-                // マージン 中央揃えで設定
-                m="auto"
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl={sampleImageUrl}
+                userName={user.name}
+                fullName={user.username}
               />
-              <Text fontSize="lg" fontWeight="bold">
-                藤原
-              </Text>
-              <Text fontSize="sm" color="gray">
-                Kakeru Fujiwara
-              </Text>
-            </Stack>
-          </Box>
-        </WrapItem>
-      </Wrap>
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
     </>
   );
 });
